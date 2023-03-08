30dc4ca1ff431ec2cbf4373c8b8cdfae234ec796
JackNDS <jacknds@hotmail.com>
2023-03-08 12:32:49 +1100

    update8march

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const mobilebtn = document.querySelector('.mobile-btn');
 const menu = document.getElementById('mobile-menu');
 const mural = document.querySelector('.mural');
 const body = document.querySelector('body');
-const swup = new Swup();
 
 
 let vh = window.innerHeight * 0.01;
