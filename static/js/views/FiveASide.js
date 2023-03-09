01056e6543a12c9f9921762f7dd2f0a8a217e8cb
JackNDS <jacknds@hotmail.com>
2023-03-10 09:39:30 +1100

    draft 2.1

--- static/js/views/FiveASide.js
+++ static/js/views/FiveASide.js
@@ -47,6 +47,33 @@ export default class extends AbstractView {
           </div>
     
         </div>
+
+        <div class="sub-page">
+        <h1 class="drop-shadow-lg">WHY PLAY AT WANDERERS FIVES?</h1>
+        <p class="drop-shadow-lg">
+
+        Accessible divisions and evenly matched competitions
+        <br><br>
+        Use it as an extra training run to compliment your club football.
+        <br><br>
+        Register as a social group with no affiliation to any club or association
+        <br><br>
+        Personal Accident Cover for all registered participants in line with the sports governing body<br><br>
+
+        <h1 class="drop-shadow-lg">HOW ITS PLAYED</h1><br><br>
+
+        Field size 33m x 20m
+        <br><br>
+5 players on the pitch including a keeper.
+<br><br>
+No out of bounds and modified goals
+<br><br>
+Unlimited subs with up to 10 players per team
+<br><br>
+No slide tackles permitted.<br><br>
+</p>
+<a href="/five-a-side"><button class="btn mt-6 shadow-lg rounded-full py-2 sm:py-4 px-6 hover:translate-y-1 transition-transform duration-500 ease-in-out text-2xl tracking-wide"">Regsiter Here</button></a>
+</div>
         `;   
     }
 }
