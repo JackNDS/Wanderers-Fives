9e7f6165c9c3edec46769401f181ef69fdbad0f4
JackNDS <jacknds@hotmail.com>
2023-03-10 15:52:11 +1100

    draft 2.1

--- static/js/index.js
+++ static/js/index.js
@@ -6,6 +6,7 @@ import PitchHire from "./views/PitchHire.js";
 import FiveASide from "./views/FiveASide.js";
 import Terms from "./views/Terms.js";
 import Parties from "./views/Parties.js";
+import Contact from "./views/Contact.js";
 
 const navigateTo = url => {
     history.pushState(null, null, url);
@@ -22,6 +23,7 @@ const router = async () => {
        { path: "/home", view: Home },
        { path: "/terms", view: Terms },
        { path: "/parties", view: Parties},
+       { path: "/contact", view: Parties},
     ];
     // Test each route for potential match
     const potentialMatches = routes.map(route => {
