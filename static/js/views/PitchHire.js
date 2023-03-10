4f0c4055c52f4ed1de1be244ed3d66049a7afa5c
JackNDS <jacknds@hotmail.com>
2023-03-10 15:48:32 +1100

    draft 2.1

--- static/js/views/PitchHire.js
+++ static/js/views/PitchHire.js
@@ -11,16 +11,17 @@ export default class extends AbstractView {
         <div class="pitch-hire">
         <div class="sub-page pitchhire">
         <h1 class="drop-shadow-lg">PITCH HIRE</h1>
-        <p class="drop-shadow-lg">
 <div class="hire-container">
 <div class="hire1">
+    <p class="drop-shadow-lg">
 Opening Hours:<br><br>
 Monday to Friday 10:00am to 11:00pm<br>
 Saturday and Sunday: 8:00am to 10:30pm<br><br>
-Long term club bookings for 2023 are open now.<br><br>
+Long term club bookings for 2023 are open now.<br><br></p>
 </div>
 
 <div class="hire2">
+    <p class="drop-shadow-lg">
 Rates:<br><br>
 Monday to Thursday (Peak): 6pm - 11pm<br>
 $110/hour<br><br>
@@ -28,9 +29,10 @@ All other opening hours (Off Peak):<br>
 $90/hour<br><br>
 Schools:<br>
 $90/hour<br><br>
+</p>
 </div>
 </div>
-
+<p class="drop-shadow-lg">
 <a href="mailto:CMckibbins@wswanderersfc.com.au" class="underline"> Email Wanderers Fives</a></p><br><br>
 
 <iframe id="book" class="bg-white book" src="https://wanderers.duktag.net/duktag/schedule.cfm?&court=990,991,992,993,994,995,996,997,998" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
