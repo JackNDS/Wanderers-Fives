4cd489234bd17b439e580e5ccdc4f79b257acf91
JackNDS <jacknds@hotmail.com>
2023-03-13 17:16:30 +1100

    13march2023

--- static/js/views/Dashboard.js
+++ static/js/views/Dashboard.js
@@ -3,7 +3,7 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
     constructor() {
       super();
-        this.setTitle("Dashboard");
+        this.setTitle("Wanderers Fives");
     }
 
     async getHtml() {
