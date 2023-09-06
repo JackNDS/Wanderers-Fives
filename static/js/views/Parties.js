df8af08302150f6960e5c4be56c40a77878b7a1f
JackNDS <jacknds@hotmail.com>
2023-09-06 15:01:37 +1000

    update 170823

--- static/js/views/Parties.js
+++ static/js/views/Parties.js
@@ -11,7 +11,7 @@ export default class extends AbstractView {
         <div class="sub-page">
         <h1 class="drop-shadow-lg">BIRTHDAY PARTIES</h1>
         
-        <p>All of our birthday parties booked before July 1st will include a JD Sports Voucher to the value of $100 for the birthday child.<br><br>
+        <p>
         In the request form below, please include all details/specifics for your event in the message field.</p><br><br>
 
         <a href="/party-form" target="popup" onclick="window.open('party-form','popup','width=1000,height=1000,left=100,top=100'); return false;"><button class="btn -ml-1 shadow-lg rounded-full py-2 sm:py-4 px-6 hover:translate-y-1 transition-transform duration-500 ease-in-out text-2xl tracking-wide"">Book a Party</button></a>
