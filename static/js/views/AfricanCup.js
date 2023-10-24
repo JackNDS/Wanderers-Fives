6c93ae197ca56199087af86a7bf26beca4e3030b
JackNDS <jacknds@hotmail.com>
2023-10-25 09:59:12 +1100

    update 280923

--- static/js/views/AfricanCup.js
+++ static/js/views/AfricanCup.js
@@ -12,13 +12,13 @@ export default class extends AbstractView {
     <div class="explore-container">
         <div class="explore-heading font-everlooser text-white">Fives Portal</div>
         <div class="explore">
-        <a href="https://wanderers.duktag.net/duktag/team.cfm?location=278&create=1" target="popup" onclick="window.open('https://wanderers.duktag.net/duktag/team.cfm?location=278&create=1','popup','width=600,height=800,left=400,top=100'); return false;"><div class="card font-everlooser text-black">
+        <a href="https://wanderers.duktag.net/duktag/team.cfm?location=355&create=1" target="popup" onclick="window.open('https://wanderers.duktag.net/duktag/team.cfm?location=355&create=1','popup','width=600,height=800,left=400,top=100'); return false;"><div class="card font-everlooser text-black">
             CREATE A TEAM
           </div></a>
-          <a href="https://wanderers.duktag.net/duktag/register.cfm?location=278&team=1" target="popup" onclick="window.open('https://wanderers.duktag.net/duktag/register.cfm?location=278&team=1','popup','width=600,height=800,left=400,top=100'); return false;"><div class="card font-everlooser text-WSWRed">
+          <a href="https://wanderers.duktag.net/duktag/register.cfm?location=355&team=1" target="popup" onclick="window.open('https://wanderers.duktag.net/duktag/register.cfm?location=355&team=1','popup','width=600,height=800,left=400,top=100'); return false;"><div class="card font-everlooser text-WSWRed">
             JOIN A TEAM
           </div></a>
-          <a href="https://wanderers.duktag.net/duktag/team.cfm?location=278" target="popup" onclick="window.open('https://wanderers.duktag.net/duktag/team.cfm?location=278','popup','width=600,height=800,left=400,top=100'); return false;"><div class="card font-everlooser text-black">
+          <a href="https://wanderers.duktag.net/duktag/team.cfm?location=355" target="popup" onclick="window.open('https://wanderers.duktag.net/duktag/team.cfm?location=355','popup','width=600,height=800,left=400,top=100'); return false;"><div class="card font-everlooser text-black">
             TEAM CAPTAIN LOGIN
           </div></a>
           <a href="https://wanderers.duktag.net/duktag/checkout.cfm" target="popup" onclick="window.open('https://wanderers.duktag.net/duktag/checkout.cfm','popup','width=600,height=800,left=400,top=100'); return false;"><div class="card font-everlooser text-WSWRed">
