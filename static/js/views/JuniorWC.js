e54669356295b7ed21c2daa1ec629a829887a271
JackNDS <jacknds@hotmail.com>
2023-11-24 12:56:08 +1100

    update 241123

--- static/js/views/JuniorWC.js
+++ static/js/views/JuniorWC.js
@@ -30,7 +30,46 @@ export default class extends AbstractView {
         <img src="/img/juniorworldcup.png" alt="">
       </div>
 
-  
+      <div class="sub-page">
+      <h1>EXPRESSION OF INTEREST</h1>
+          <div class="party-container">
+          <form id="contact-form" name="Contact" method="POST" netlify-honeypot="bot-field" data-netlify="true" onsubmit="submitForm(event)" >
+              <input type="hidden" name="form-name" value="Contact">
+                   <p class="hidden">
+                  <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
+                  </p>
+              <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" required>
+              </div>
+              <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+              </div>
+              <div class="form-group">
+                <label for="phone">Mobile:</label>
+                <input type="tel" id="phone" name="phone" required>
+              </div>
+              <div class="form-group">
+                <label for="message">Message:</label>
+                <textarea id="message" name="message"></textarea>
+              </div>
+              <div class="submit">
+                <input type="submit" value="Submit">
+              </div>
+            </form>  
+          </div>
+          <div class="contact-info">
+            <div class="contact-email">
+             <a href="mailto:fives@wswanderersfc.com.au">
+             <i class="fas fa-envelope"></i>fives@wswanderersfc.com.au</a>
+            </div>
+            <div class="contact-phone">
+              
+             <a href="tel:86026411"><i class="fas fa-phone"></i> 86026411</a>
+            </div>
+          </div>
+      </div>
       </div>
       </div>
 `;   
