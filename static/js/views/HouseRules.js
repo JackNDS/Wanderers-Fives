d3c4d2641a96f58e54e8d8018f41acb8d348cf9a
JackNDS <jacknds@hotmail.com>
2024-01-11 18:46:51 +1100

    update 010124

--- static/js/views/HouseRules.js
+++ static/js/views/HouseRules.js
@@ -61,35 +61,6 @@ export default class extends AbstractView {
 26.	Wanderers Fives staff may stop games at any time to identify participants on the team list. Any player unable to prove their identity will result in the match being forfeited.<br><br><br>
             
   </p>
-  
-        <h1 id="jdcomp" class="drop-shadow-lg">JD Shootout Competition Rules</h1>
-  
-        <p class="drop-shadow-lg">
-        Field size 33m x 20m
-        <br><br>
-        1.	All teams pay $150 to enter the tournament. All players must be registered prior to the 29th of March. Registration can be completed online or manually added via competition admin staff at Wanderers 5s<br><br>
-        2.	No player registration will be accepted on the day of the tournament March 29th 2023<br><br>
-        3.	Teams will be drawn into Pools on Wednesday 29th March by an A League Mens Player under the supervision of Wanderers CEO Scott Hudson<br><br>
-        4.	Fixtures will be completed and sent to teams Wednesday 29th March 2023<br><br>
-        5.	Teams will play in 3 pools of 4 teams <br><br>
-        6.	Teams will play 3 pool games 2 x 10 minute halves, 4 minute half time. No stoppage or injury time. All pool games will be played back-to-back in a 90 minute period<br><br>
-        7.	Games will be governed by Wanderers 5s Competition Rules and House Rules found <a href="/five-a-side" target="popup" class="underline">here</a> with the following exceptions.<br>
-        -	No substitutions in the final 2 minutes of any half<br>
-        -	Teams cannot substitute a goalkeeper during play (half time only). Referee can adjudicate in the event of Injury.<br>
-        -	The ball is not required to stay below the height of the rebound boards during the JD Shootout, the ball can be played and rebound off all surfaces. Any ball that exits the playing area will be restarted by the keeper of the team who did not cause the ball to leave the pitch.<br>
-        -	Players receiving two yellow cards throughout the tournament will be dismissed for the remainder of the tournament. This includes an immediate dismissal in any game that the player receives the second yellow for the tournament.<br>
-        -	Any red card the player is dismissed from the game and the tournament immediately.<br>
-        -	Red carded players are ineligible to play any part in the game BUT teams may substitute and continue without playing a player short<br><br>
-        
-        8.	Pool games will receive 3 points for a win, 1 point for a draw and 0 points for a loss. From the 3 pools the top 2 teams will advance and be identified by pool and seed from that pool.  Ie A1, A2, B1, B2, C1,C2.<br><br>
-        9.	Teams qualifying as the Lucky Loser will be ranked LL1 and LL2 based on standard FIFA criteria applied across the remaining 6 teams (all pools) . First by total Competition points, then combined goal difference, then total goals scored, then relative conduct (yellow card 1 deduction, any red card  the team is ineligible to proceed via tie break, teams remaining tied after that criteria will be decided by coin toss) <br><br>
-        10.	A1,B1 and C1 as top seeds will draw opponents from the remaining pool of unseeded opponents ( A2, B2,C2, LL1, LL2). Once Seeded teams have drawn opponents the remaining (2) unseeded teams form the last game in the Knock out stage. Matches will be placed into quarter final positions with the highest ranked seeded team on one side of the draw. The remaining two seeded teams will be on the opposite side of the draw.  The last knock out game featuring unseeded teams will be placed on the same side as the highest ranked seeded team.<br><br>
-        11.	 Teams will progress via elimination to Semi final and Final. The semi final losing teams will play off for 3rd place prize<br><br>
-        12.	 The winning team will be awarded $1000 in vouchers from JD Sports, The runner up receives $600  and the 3rd placed team $250 in vouchers. <br><br>
-        
-  
-  </p>
-  </div>
   </div> `;   
     }
 }
