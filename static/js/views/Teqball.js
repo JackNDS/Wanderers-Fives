d60ec609ee9b534c4621d81c7c054b8d59199d5d
JackNDS <jacknds@hotmail.com>
2024-05-31 17:06:25 +1000

    310524

--- static/js/views/Teqball.js
+++ static/js/views/Teqball.js
@@ -23,7 +23,7 @@ export default class extends AbstractView {
 
         Teqball is a football-based sport played on a specially curved table, combining elements of football with elements of table tennis. Founded in Budapest, Hungary in 2014, Teqball has gained popularity worldwide for its unique gameplay and accessibility. 
 <br><br>
-<div><iframe width="560" height="315" src="https://www.youtube.com/embed/HG25k7kJxUI?si=vGbICPDxmo0XM5WC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
+<div class="teqball-video"><iframe width="560" height="315" src="https://www.youtube.com/embed/HG25k7kJxUI?si=vGbICPDxmo0XM5WC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
 </p>
 </div>
 
