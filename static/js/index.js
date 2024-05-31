a065b9acbcaaaf99212b6c578ba917b762c26277
JackNDS <jacknds@hotmail.com>
2024-05-31 16:14:17 +1000

    280524

--- static/js/index.js
+++ static/js/index.js
@@ -15,7 +15,7 @@ import AfricanCup from "./views/AfricanCup.js";
 import Copa from "./views/Copa.js";
 import JuniorWC from "./views/JuniorWC.js";
 import AsianCup from "./views/AsianCup.js";
-import TeqBall from ".views/TeqBall.js";
+import Teqball from "./views/teqball.js";
 
 const navigateTo = url => {
     history.pushState(null, null, url);
@@ -41,9 +41,7 @@ const router = async () => {
        { path: "/copa", view: Copa},
        { path: "/junior-worldcup", view: JuniorWC},
        { path: "/asiancup", view: AsianCup},
-       { path: "/teqball", view: TeqBall},
-    
-       
+       { path: "/teqball", view: Teqball},
 
 
     ];
